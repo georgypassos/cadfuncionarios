b362d47f132f7d3cc8e2797f2a6b9e1060a476e2
Georgy Passos <georgy1101@gmail.com>
2021-01-22 22:22:33 -0300

    Primeiro commit

---
+++ src/main/java/com/capgemini/georgy/cadfuncionarios/CadfuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.georgy.cadfuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadfuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadfuncionariosApplication.class, args);
+	}
+
+}
